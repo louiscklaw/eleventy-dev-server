151c3bc705ce3cc0b9a23d1fe2b38c9b6ef2ec30
louiscklaw <logickee@gmail.com>
2023-03-17 15:11:49 +0800

    update,

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ class EleventyDevServer {
         },
 
         // add usePolling for using windows with docker
-        usePolling: true
+        alwaysStat: true
       });
 
       this._watcher.on("change", (path) => {
